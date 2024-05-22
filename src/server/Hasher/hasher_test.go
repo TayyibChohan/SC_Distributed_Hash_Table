15bdfc525145202f57273a8deac9fc5279d41efc
Tayyib Chohan <tayyibchohan@gmail.com>
2024-05-22 12:03:46 -0700

    init boiler plate

---
+++ src/server/Hasher/hasher_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHasher(t *testing.T) {
+	//TODO: Implement TestHasher
+}
---
+++ src/server/serverConstants.go
@@ -0,0 +1,10 @@
+package server
+
+// "time"
+
+const (
+	//TODO: Declare all the constants here
+	// Server constants
+
+	MAXIMUM_TOTAL_KV_SIZE = 1000000
+)
